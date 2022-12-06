c8fe8125c4a119bbc693b90a0461151511e7b344
sanjaykumbhar26 <sanjay.webidea@gmail.com>
2022-12-06 14:02:02 +0530

    html app

---
+++ projects/html-app/assets/js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+ 
+  $("#owl-example").owlCarousel({
+        navigation: false, 
+        touchDrag: true,
+        slideSpeed : 1300,
+        paginationSpeed : 400,
+        singleItem: true,
+        pagination: false,
+        rewindSpeed: 500
+	});
+ 
+});
